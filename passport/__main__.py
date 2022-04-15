6b4233302a800afb2630ab36c44d075834850487
Jeff Epler <jepler@gmail.com>
2022-04-15 08:59:33 -0500

    Add --output-dir

--- passport/__main__.py
+++ passport/__main__.py
@@ -19,13 +19,17 @@ __progname__ = "passport"
 @click.help_option()
 @click.version_option(version=__version__)
 @click.option("--debug", "-d", is_flag="True", help="print debugging information while processing")
+@click.option("--output-dir", "output_dir", type=click.Path(file_okay=False, dir_okay=True), default=None, help="Output directory")
 @click.option("--output", "-o", "output_file", type=click.Path(), default=None, help="Output path, defaults to the input with the extension replaced with .woz. When multiple input files are specified, --output may not be used.")
 @click.option("--overwrite/--no-overwrite", "-f/-n", default=False, help="Controls whether to overwrite an output file. Files are not overwritten by default.")
 @click.argument("input-files", type=click.Path(exists=True), nargs=-1)
-def main(debug, input_files, output_file, overwrite):
+def main(debug, input_files, output_file, output_dir, overwrite):
     "Convert a disk image to .woz format with minimal processing"
     logger = debug and DebugLogger or DefaultLogger
     
+    if output_file is not None and output_dir is not None:
+        raise SystemExit("--output and --output-dir are mutually exclusive")
+
     if len(input_files) == 1:
         if output_file is None:
             output_file = os.path.splitext(input_files[0])[0] + ".woz"
@@ -36,6 +40,9 @@ def main(debug, input_files, output_file, overwrite):
     elif output_file is not None:
         raise SystemExit(f"--output is only valid with one input file")
 
+    if output_dir is not None:
+        os.makedirs(output_dir, exist_ok=True)
+
     for input_file in input_files:
         base, ext = os.path.splitext(input_file)
         ext = ext.lower()
@@ -45,6 +52,9 @@ def main(debug, input_files, output_file, overwrite):
             reader = a2rimage.A2RImage
         else:
             raise SystemExit("unrecognized file type")
+
+        if output_dir:
+            output_file = os.path.join(output_dir, os.path.splitext(os.path.basename(input_file))[0] + ".woz")
 
         logger = debug and DebugLogger or DefaultLogger
         with open(input_file, "rb") as f:
